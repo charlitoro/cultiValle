d53d0a9bf22b800a7b414d3daae4af7ef4dbf263
Andres Toro <carlostoro04@gmail.com>
2020-02-09 23:16:03 -0500

    add gitlab repository

--- app/src/main/java/com/example/exportationapp/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/exportationapp/ui/about/AboutFragment.java
@@ -1,6 +1,8 @@
 package com.example.exportationapp.ui.about;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,21 +19,36 @@ import mehdi.sakout.aboutpage.Element;
 public class AboutFragment extends Fragment {
 
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
         Element versionElement = new Element();
         versionElement.setTitle(getString(R.string.version)+" 1.1");
+
+        String userGitLab = "charlietoro_";
+        String repoGitLab = "cultivalle";
+        Element gitLabElement = new Element();
+        gitLabElement.setTitle("gitlab.com/charlietoro_/cultivalle");
+        gitLabElement.setIconDrawable(R.drawable.ic_icons8_color);
+        gitLabElement.setIconTint(R.color.colorGitLab);
+        Intent gitLabIntent = new Intent();
+        gitLabIntent.setAction(Intent.ACTION_VIEW);
+        gitLabIntent.addCategory(Intent.CATEGORY_BROWSABLE);
+        gitLabIntent.setData(Uri.parse(String.format("https://gitlab.com/%s/%s", userGitLab, repoGitLab)));
+
+        gitLabElement.setIntent(gitLabIntent);
+
         View aboutPage = new AboutPage(getContext())
                 .setDescription(getString(R.string.description))
                 .isRTL(false)
                 .setImage(R.drawable.ic_agro_export_app)
                 .addItem(versionElement)
-                .addGroup(getString(R.string.connect_us))
                 .addEmail("carlostoro04@gmail.com")
-                .addFacebook("charlietoro")
+                .addFacebook("charlietoroag")
                 .addTwitter("charlietoro_")
                 .addInstagram("charlietoro_")
+                .addItem(gitLabElement)
                 .create();
         return aboutPage;
     }
